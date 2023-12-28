56d1a6529aa1585a874be735377a8805422899fd
JaduTermux <JaduTermux@gmail.com>
2023-12-29 00:09:35 +0800

    added suggestions

--- src/searchview.py
+++ src/searchview.py
@@ -10,9 +10,10 @@ class SearchView():
     self.suggestions = ft.Ref[ft.Column]();
     self.suggestcontainer = ft.Ref[ft.Container]();
     
-  def new(self):
-    return ft.Container(ref = self.suggestcontainer, alignment = ft.alignment.center, content = ft.Column(ref = self.suggestions, controls = [
-      ft.TextField(width = percent(80, self.page.width), label = "Search", hint_text = "Jojo Music", on_change = self.suggest),
+  def new(self): # fix this
+    return ft.Container(width = percent(100, self.page.width), height = percent(100, self.page.height), content = ft.Column(controls = [
+      ft.Container(alignment = ft.alignment.center, content = ft.TextField(width = percent(80, self.page.width), label = "Search", hint_text = "Jojo Music", on_change = self.suggest)),
+      ft.Container(ref = self.suggestcontainer, content = ft.Column(ref = self.suggestions, width = percent(100, self.page.width), height = percent(50, self.page.height), scroll = ft.ScrollMode.ALWAYS)),
     ]));
 
   def redirect_player(self, e):
@@ -20,6 +21,12 @@ class SearchView():
 
   def suggest(self, e):
     text = e.control.value;
+    self.suggestions.current.controls.clear();
     if len(text) > 0:
       for music in Music().search(text):
-        pass;
+        self.suggestions.current.controls.append(ft.Text(music["title"]));
+      self.suggestcontainer.current.visible = True;
+    else:
+      self.suggestcontainer.current.visible = False;
+    
+    self.page.update();
